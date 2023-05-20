bb6651f016a7579c6ca829d3f97f7edbb08a6af1
Shubham <shubhamkahar.ele16@gmail.com>
2023-05-20 19:10:06 +0530

    Completed

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+    // Empty class since we don't need any persistent storage for this simple application
+}
